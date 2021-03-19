6513ace7f8e0da5d442049c4cfd3a748f374f0a4
PolyDevil <polydevil@cocaine.ninja>
2021-03-19 14:46:02 +0300

    Build

---
+++ lib/helpers.d.ts
@@ -0,0 +1,1 @@
+export declare const getClientX: (e: MouseEvent | TouchEvent) => number;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="react" />
-import { Props, Value } from './types';
-declare const useScrubbler: ({ value: initialValue, min, max, step, isInteger, toFixed, className, }: Props) => {
+import { Props, Value } from "./types";
+declare const useScrubbler: ({ value: initialValue, min, max, step, isInteger, toFixed, className }: Props) => {
     value: Value;
     onChange: import("react").Dispatch<import("react").SetStateAction<Value>>;
-    onMouseDown: ({ clientX }: React.MouseEvent) => void;
+    onScrubble: (e: React.MouseEvent | React.TouchEvent) => void;
+    getScrubbler: () => {
+        onMouseDown: (e: React.MouseEvent | React.TouchEvent) => void;
+        onTouchStart: (e: React.MouseEvent | React.TouchEvent) => void;
+    };
 };
 export default useScrubbler;
